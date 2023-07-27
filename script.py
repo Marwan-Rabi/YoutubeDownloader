27cb3822a4f67735285f18d8a200bcca7ef0ce73
Marwan Rabi <106484451+Marwan-Rabi@users.noreply.github.com>
2023-07-27 11:49:35 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+#importing files
+from pytube import YouTube
+import cv2
+
+#input video link and Path
+Vlink = input("Enter Video Link: ")
+VPAth  = input("Enter The Path of The Download")
+video = YouTube(Vlink)
+#print video descriptions
+
+print(f"Title: {video.title}")
+print(f"Length: {float(video.length / 60)} min")
+print(f"Views: {video.views}")
+
+#set resolution
+videoStream = video.streams.get_highest_resolution()
+
+#Download
+print("Downloading....")
+videoStream.download(VPAth)
+print("Downloading finished!!")
+cv2.VideoCapture(video.title.mp4)
+#quit
+quit()
+
